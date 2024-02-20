d9e0ce14bda69ac58b4b56a75ad44b86695525c1
TheRealHex <devilxnp@gmail.com>
2024-02-20 17:26:35 +0545

    init: commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Main());
+}
+
+class Main extends StatelessWidget {
+  const Main({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather App',
+      theme: ThemeData(
+          // useMaterial3: true,
+          ),
+    );
+  }
+}
